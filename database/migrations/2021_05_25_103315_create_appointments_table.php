023d97a0c75e2be3970f808deea8606536e2e350
shaymaa khalaf <shaymaa.khalaf91@gmail.com>
2021-06-12 13:51:05 +0300

    ajax changes

---
+++ database/migrations/2021_05_25_103315_create_appointments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->string('fullname');
+            $table->string('department');
+            $table->string('doctor');
+            $table->date('date');
+            $table->string('phone');
+            $table->text('notes');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
